e6eeecb6af425d3edb36edd4de7d59bea50366aa
Ziad <ziad1991@hotmail.co.uk>
2020-01-13 20:38:13 +0000

    convert calls into methods

--- directory.rb
+++ directory.rb
@@ -11,10 +11,18 @@ students = [
 "Joffrey Baratheon",
 "Norman Bates"
 ]
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-
-puts "Overall, we have #{students.count} great students"
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+print_header
+print(students)
+print_footer(students)
