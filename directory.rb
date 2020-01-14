62adf12710b7a416382965970a35128870e0fb18
Ziad <ziad1991@hotmail.co.uk>
2020-01-14 23:16:26 +0000

    remove calls and replace with interactive menu method

--- directory.rb
+++ directory.rb
@@ -1,3 +1,29 @@
+def interactive_menu
+  students = []
+  loop do
+  # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+  # 2. read the input and save it into a variable
+    selection = gets.chomp
+  # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      # show the students
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -17,23 +43,22 @@ def input_students
   students
 end
 
+# prints the header lines
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
+# prints students and their cohort
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} - (#{student[:cohort]} cohort)"
   end
 end
 
+# prints a footer line saying the number of students
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.length} great students"
 end
 
-students = input_students
-# nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
+interactive_menu
